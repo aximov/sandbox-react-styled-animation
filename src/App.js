80a5b4430269d490410242be5bb6c1c793f5e72e
Akimoto Daichi <deltalphan@gmail.com>
2020-03-31 02:18:40 +0900

    snapshot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const click = event => {
+  render(); // 再描画
+};
+
+const Block = styled.div`
+  width: 100pt;
+  height: 100pt;
+  margin: 20pt;
+  background-color: chocolate;
+`
+
+const AnimationBlock = props => (
+  <div>
+    <Block>
+      ブロック
+    </Block>
+    <button id="scaling-button" onClick={click}>
+      拡大縮小
+    </button>
+  </div>
+);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnimationBlock />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import styled from 'styled-components'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const box = { width: "100pt", height: "100pt" };
+
+const click = event => {
+  render(); // 再描画
+};
+
+const Block = styled.div`
+  width: 100pt;
+  height: 100pt;
+  margin: 20pt;
+  background-color: chocolate;
+`
+
+// コンポーネント定義
+const AnimationBlock = props => (
+  <div>
+    <Block>
+      ブロック
+    </Block>
+    <button id="scaling-button" onClick={click}>
+      拡大縮小
+    </button>
+  </div>
 );
+
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <AnimationBlock />
+    </React.StrictMode>
+    ,
+    document.getElementById("root")
+  );
+};
+
+render();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
